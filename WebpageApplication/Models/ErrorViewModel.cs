e868b2f59d2eebebbc4e6145963175742a9078c7
santhosh <santhoshcellamuthuc@gmail.com>
2024-07-08 21:01:22 +0530

    Add project files.

---
+++ WebpageApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebpageApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
